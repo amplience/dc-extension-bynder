72b65a3aaac46151914cd4f1ceb26470711bcae2
Ben Pearey <bpearey@amplience.com>
2023-11-08 10:23:11 +0000

    feat: allow content item data mapping - wip

--- src/components/BynderExtension.tsx
+++ src/components/BynderExtension.tsx
@@ -1,20 +1,45 @@
 import React, { useState } from "react";
 import { useContentFieldExtension } from "./WithFieldExtension";
 import BynderImageField from "./BynderImageField";
+import jsonpath from 'jsonpath';
 
 function BynderExtension() {
   const sdk = useContentFieldExtension();
   const [value, setValue] = useState(sdk.initialValue);
   const [openDialog, setOpenDialog] = useState(false);
 
+  const defaultMapping: Record<string, {jsonPath: string}> = {
+    "name": { jsonPath: "$.name" },
+    "files": { jsonPath: "$.files" },
+    "databaseId": { jsonPath: "$.databaseId" }
+  };
+
+  //@ts-ignore
+  const installationMappings = sdk.params.installation?.mapping;
+
+  const mappings: Record<string, {jsonPath: string}> = installationMappings ? {
+    ...installationMappings,
+    ...defaultMapping
+  } : null;
+
+  const valueMapper = (value, mappings: Record<string, {jsonPath: string}>) => {
+    let mappedValues = {};
+    for (const [key, mapping] of Object.entries(mappings)) {
+      if (value) {
+        mappedValues[key] = jsonpath.query(value, mapping.jsonPath)[0];
+      }
+    }
+    return mappedValues;
+  }
+
   const handleChange = (newValue) => {
-    setValue(newValue);
-    sdk.field.setValue(newValue);
+    let mappedValues = mappings ? valueMapper(newValue, mappings) : newValue;
+    setValue(mappedValues);
+    sdk.field.setValue(mappedValues);
   };
 
   const handleOpenDialog = () => {
     setOpenDialog(true);
-
     (window as any).BynderCompactView.open({
       language: "en_US",
       theme: {
