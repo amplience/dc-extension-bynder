3a71453a339cff8c744fef6870586d448cfa73b2
Ben Pearey <bpearey@amplience.com>
2023-11-16 13:55:34 +0000

    feat: check for duplicate items on replace action (#16)
    * feat: check for duplicate items on replace action
    
    * feat: adding alert dialog props type

---
+++ src/components/AlertDialog.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+export type AlertDialogProps = {
+  open: boolean;
+  title: string;
+  text: string;
+  handleClose: () => void;
+};
+
+export default function AlertDialog({ open, handleClose, title, text }: AlertDialogProps) {
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">{text}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} autoFocus>
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
